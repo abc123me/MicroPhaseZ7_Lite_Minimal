ab53fd96341f8dac85810f9c72e409d8f02c0cf1
jeremiah <jeremiahbl@protonmail.com>
2026-02-23 22:43:22 -0600

    initial commit of vivado project

---
+++ MicroPhaseZ7_Lite_Minimal.gen/sources_1/bd/minimal/hdl/minimal_wrapper.v
@@ -0,0 +1,167 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.2 (lin64) Build 6299465 Fri Nov 14 12:34:56 MST 2025
+//Date        : Mon Feb 23 22:35:08 2026
+//Host        : npc running 64-bit Arch Linux
+//Command     : generate_target minimal_wrapper.bd
+//Design      : minimal_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module minimal_wrapper
+   (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    ETH_MDIO_mdc,
+    ETH_MDIO_mdio_io,
+    ETH_MII_col,
+    ETH_MII_crs,
+    ETH_MII_rst_n,
+    ETH_MII_rx_clk,
+    ETH_MII_rx_dv,
+    ETH_MII_rx_er,
+    ETH_MII_rxd,
+    ETH_MII_tx_clk,
+    ETH_MII_tx_en,
+    ETH_MII_txd,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    pl_key_2,
+    pl_led_2);
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+  output ETH_MDIO_mdc;
+  inout ETH_MDIO_mdio_io;
+  input ETH_MII_col;
+  input ETH_MII_crs;
+  output ETH_MII_rst_n;
+  input ETH_MII_rx_clk;
+  input ETH_MII_rx_dv;
+  input ETH_MII_rx_er;
+  input [3:0]ETH_MII_rxd;
+  input ETH_MII_tx_clk;
+  output ETH_MII_tx_en;
+  output [3:0]ETH_MII_txd;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  input pl_key_2;
+  output pl_led_2;
+
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire ETH_MDIO_mdc;
+  wire ETH_MDIO_mdio_i;
+  wire ETH_MDIO_mdio_io;
+  wire ETH_MDIO_mdio_o;
+  wire ETH_MDIO_mdio_t;
+  wire ETH_MII_col;
+  wire ETH_MII_crs;
+  wire ETH_MII_rst_n;
+  wire ETH_MII_rx_clk;
+  wire ETH_MII_rx_dv;
+  wire ETH_MII_rx_er;
+  wire [3:0]ETH_MII_rxd;
+  wire ETH_MII_tx_clk;
+  wire ETH_MII_tx_en;
+  wire [3:0]ETH_MII_txd;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire pl_key_2;
+  wire pl_led_2;
+
+  IOBUF ETH_MDIO_mdio_iobuf
+       (.I(ETH_MDIO_mdio_o),
+        .IO(ETH_MDIO_mdio_io),
+        .O(ETH_MDIO_mdio_i),
+        .T(ETH_MDIO_mdio_t));
+  minimal minimal_i
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .ETH_MDIO_mdc(ETH_MDIO_mdc),
+        .ETH_MDIO_mdio_i(ETH_MDIO_mdio_i),
+        .ETH_MDIO_mdio_o(ETH_MDIO_mdio_o),
+        .ETH_MDIO_mdio_t(ETH_MDIO_mdio_t),
+        .ETH_MII_col(ETH_MII_col),
+        .ETH_MII_crs(ETH_MII_crs),
+        .ETH_MII_rst_n(ETH_MII_rst_n),
+        .ETH_MII_rx_clk(ETH_MII_rx_clk),
+        .ETH_MII_rx_dv(ETH_MII_rx_dv),
+        .ETH_MII_rx_er(ETH_MII_rx_er),
+        .ETH_MII_rxd(ETH_MII_rxd),
+        .ETH_MII_tx_clk(ETH_MII_tx_clk),
+        .ETH_MII_tx_en(ETH_MII_tx_en),
+        .ETH_MII_txd(ETH_MII_txd),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .pl_key_2(pl_key_2),
+        .pl_led_2(pl_led_2));
+endmodule
---
+++ io_1.v
@@ -0,0 +1,118 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2025.2 (lin64) Build 6299465 Fri Nov 14 12:34:56 MST 2025
+// Date        : Sat Feb 21 20:30:40 2026
+// Host        : npc running 64-bit Arch Linux
+// Command     : write_verilog -mode pin_planning -force -port_diff_buffers
+//               /home/jeremiah/Programming/FPGA/Vivado/MicroPhaseZ7_Lite_Minimal/io_1.v
+// Design      : ios
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+module ios(ETH_RXD, ETH_TXD, ETH_TXCTL, PL_CLK_50M, PL_KEY1, PL_KEY2, PL_LED1, PL_LED2, HDMI1_CLK_N, HDMI1_CLK_P, HDMI1_D0_N, HDMI1_D0_P, HDMI1_D1_N, HDMI1_D1_P, HDMI1_D2_N, HDMI1_D2_P, HDMI1_HPD, HDMI1_I2C_SCL, HDMI1_I2C_SDA, ETH_RXCK, ETH_RXDV, ETH_TXCK, ETH_nRST, ETH_MDIO_mdio_io, ETH_MDIO_mdc, GPIO1_0P, GPIO1_0N, GPIO1_1P, GPIO1_1N, GPIO1_2P, GPIO1_2N, GPIO1_3P, GPIO1_3N, GPIO1_4P, GPIO1_4N, GPIO1_5P, GPIO1_5N, GPIO1_6P, GPIO1_6N, GPIO1_7P, GPIO1_7N, GPIO1_8P, GPIO1_8N, GPIO1_9P, GPIO1_9N, GPIO1_10P, GPIO1_10N, GPIO1_11P, GPIO1_11N, GPIO1_12P, GPIO1_12N, GPIO1_13P, GPIO1_13N, GPIO1_14P, GPIO1_14N, GPIO1_15P, GPIO1_15N, GPIO1_16P, GPIO1_16N, GPIO1_17P, GPIO1_17N, GPIO2_0P, GPIO2_0N, GPIO2_1P, GPIO2_1N, GPIO2_2P, GPIO2_2N, GPIO2_3P, GPIO2_3N, GPIO2_4P, GPIO2_4N, GPIO2_5P, GPIO2_5N, GPIO2_6P, GPIO2_6N, GPIO2_7P, GPIO2_7N, GPIO2_8P, GPIO2_8N, GPIO2_9P, GPIO2_9N, GPIO2_10P, GPIO2_10N, GPIO2_11P, GPIO2_11N, GPIO2_12P, GPIO2_12N, GPIO2_13P, GPIO2_13N, GPIO2_14P, GPIO2_14N, GPIO2_15P, GPIO2_15N, GPIO2_16P, GPIO2_16N, GPIO2_17P, GPIO2_17N);
+  inout [3:0] ETH_RXD;
+  inout [3:0] ETH_TXD;
+  inout [0:0] ETH_TXCTL;
+  inout PL_CLK_50M;
+  inout PL_KEY1;
+  inout PL_KEY2;
+  inout PL_LED1;
+  inout PL_LED2;
+  inout HDMI1_CLK_N;
+  inout HDMI1_CLK_P;
+  inout HDMI1_D0_N;
+  inout HDMI1_D0_P;
+  inout HDMI1_D1_N;
+  inout HDMI1_D1_P;
+  inout HDMI1_D2_N;
+  inout HDMI1_D2_P;
+  inout HDMI1_HPD;
+  inout HDMI1_I2C_SCL;
+  inout HDMI1_I2C_SDA;
+  inout ETH_RXCK;
+  inout ETH_RXDV;
+  inout ETH_TXCK;
+  inout ETH_nRST;
+  inout ETH_MDIO_mdio_io;
+  inout ETH_MDIO_mdc;
+  inout GPIO1_0P;
+  inout GPIO1_0N;
+  inout GPIO1_1P;
+  inout GPIO1_1N;
+  inout GPIO1_2P;
+  inout GPIO1_2N;
+  inout GPIO1_3P;
+  inout GPIO1_3N;
+  inout GPIO1_4P;
+  inout GPIO1_4N;
+  inout GPIO1_5P;
+  inout GPIO1_5N;
+  inout GPIO1_6P;
+  inout GPIO1_6N;
+  inout GPIO1_7P;
+  inout GPIO1_7N;
+  inout GPIO1_8P;
+  inout GPIO1_8N;
+  inout GPIO1_9P;
+  inout GPIO1_9N;
+  inout GPIO1_10P;
+  inout GPIO1_10N;
+  inout GPIO1_11P;
+  inout GPIO1_11N;
+  inout GPIO1_12P;
+  inout GPIO1_12N;
+  inout GPIO1_13P;
+  inout GPIO1_13N;
+  inout GPIO1_14P;
+  inout GPIO1_14N;
+  inout GPIO1_15P;
+  inout GPIO1_15N;
+  inout GPIO1_16P;
+  inout GPIO1_16N;
+  inout GPIO1_17P;
+  inout GPIO1_17N;
+  inout GPIO2_0P;
+  inout GPIO2_0N;
+  inout GPIO2_1P;
+  inout GPIO2_1N;
+  inout GPIO2_2P;
+  inout GPIO2_2N;
+  inout GPIO2_3P;
+  inout GPIO2_3N;
+  inout GPIO2_4P;
+  inout GPIO2_4N;
+  inout GPIO2_5P;
+  inout GPIO2_5N;
+  inout GPIO2_6P;
+  inout GPIO2_6N;
+  inout GPIO2_7P;
+  inout GPIO2_7N;
+  inout GPIO2_8P;
+  inout GPIO2_8N;
+  inout GPIO2_9P;
+  inout GPIO2_9N;
+  inout GPIO2_10P;
+  inout GPIO2_10N;
+  inout GPIO2_11P;
+  inout GPIO2_11N;
+  inout GPIO2_12P;
+  inout GPIO2_12N;
+  inout GPIO2_13P;
+  inout GPIO2_13N;
+  inout GPIO2_14P;
+  inout GPIO2_14N;
+  inout GPIO2_15P;
+  inout GPIO2_15N;
+  inout GPIO2_16P;
+  inout GPIO2_16N;
+  inout GPIO2_17P;
+  inout GPIO2_17N;
+
+  // internal wires associated with differential buffers
+
+
+  // differential buffers
+
+
+endmodule
